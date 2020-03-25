1cf9c357b705ee7c6344610aa61b72cfa2da93e4
Lonng <heng@lonng.org>
2020-03-25 18:08:15 +0800

    pkg: implement the scale-in subcommand
    Signed-off-by: Lonng <heng@lonng.org>

--- cmd/scale_in.go
+++ cmd/scale_in.go
@@ -13,15 +13,62 @@
 
 package cmd
 
-import "github.com/spf13/cobra"
+import (
+	"github.com/pingcap-incubator/tiops/pkg/meta"
+	operator "github.com/pingcap-incubator/tiops/pkg/operation"
+	"github.com/pingcap-incubator/tiops/pkg/task"
+	"github.com/pingcap/errors"
+	"github.com/spf13/cobra"
+)
 
 func newScaleInCmd() *cobra.Command {
+	var nodes []string
 	cmd := &cobra.Command{
-		Use:   "scale-in",
+		Use:   "scale-in <cluster-name>",
 		Short: "Scale in a TiDB cluster",
 		RunE: func(cmd *cobra.Command, args []string) error {
-			return cmd.Help()
+			if len(nodes) < 1 || len(args) != 1 {
+				return cmd.Help()
+			}
+			return scaleIn(args[0], nodes)
 		},
 	}
+	cmd.Flags().StringSliceVar(&nodes, "node-id", nil, "Specify the node ids")
 	return cmd
+}
+
+func scaleIn(cluster string, nodeIds []string) error {
+	metadata, err := meta.ClusterMetadata(cluster)
+	if err != nil {
+		return err
+	}
+
+	// instances by uuid
+	instances := map[string]meta.Instance{}
+
+	// make sure all nodeIds exists in topology
+	for _, component := range metadata.Topology.ComponentsByStartOrder() {
+		for _, instance := range component.Instances() {
+			instances[instance.UUID()] = instance
+		}
+	}
+
+	for _, nodeID := range nodeIds {
+		if _, found := instances[nodeID]; !found {
+			return errors.Errorf("cannot find node id '%s' in topology", nodeID)
+		}
+	}
+
+	t := task.NewBuilder().
+		SSHKeySet(
+			meta.ClusterPath(cluster, "ssh", "id_rsa"),
+			meta.ClusterPath(cluster, "ssh", "id_rsa.pub")).
+		ClusterSSH(metadata.Topology, metadata.User).
+		ClusterOperate(metadata.Topology, operator.ScaleInOperation, operator.Options{
+			DeletedNodes: nodeIds,
+		}).
+		UpdateMeta(cluster, metadata, nodeIds).
+		Build()
+
+	return t.Execute(task.NewContext())
 }
--- pkg/task/builder.go
+++ pkg/task/builder.go
@@ -70,6 +70,16 @@ func (b *Builder) ClusterSSH(spec *meta.Specification, deployUser string) *Build
 	return b
 }
 
+// UpdateMeta maintain the meta information
+func (b *Builder) UpdateMeta(cluster string, metadata *meta.ClusterMeta, deletedNodeIds []string) *Builder {
+	b.tasks = append(b.tasks, &UpdateMeta{
+		cluster:        cluster,
+		metadata:       metadata,
+		deletedNodesID: deletedNodeIds,
+	})
+	return b
+}
+
 // CopyFile appends a CopyFile task to the current task collection
 func (b *Builder) CopyFile(src, dstHost, dstPath string) *Builder {
 	b.tasks = append(b.tasks, &CopyFile{
