cc5deec65ce950d242ecae80c6e44c0ac97abd36
Lonng <heng@lonng.org>
2020-03-27 15:02:55 +0800

    fix static ip
    Signed-off-by: Lonng <heng@lonng.org>

--- docker/up.sh
+++ docker/up.sh
@@ -148,6 +148,9 @@ INFO "Running \`docker-compose build\`"
 # shellcheck disable=SC2086
 docker-compose -f docker-compose.yml ${COMPOSE} ${DEV} build
 
+docker network rm tiops
+docker network create --gateway 172.19.0.1 --subnet 172.19.0.0/16 tiops
+
 INFO "Running \`docker-compose up\`"
 if [ "${RUN_AS_DAEMON}" -eq 1 ]; then
     # shellcheck disable=SC2086
