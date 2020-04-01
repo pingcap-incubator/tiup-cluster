93b324c552109d4cfb9d117f3b75084e7ace52d4
Allen Zhong <zhongbenli@pingcap.com>
2020-04-01 15:37:41 +0800

    operator: adjust outputs to show component and port

--- pkg/operation/action.go
+++ pkg/operation/action.go
@@ -359,7 +359,10 @@ func StartComponent(getter ExecutorGetter, instances []meta.Instance) error {
 
 	for _, ins := range instances {
 		e := getter.Get(ins.GetHost())
-		log.Infof("\tStarting instance %s", ins.GetHost())
+		log.Infof("\tStarting instance %s %s:%d",
+			ins.ComponentName(),
+			ins.GetHost(),
+			ins.GetPort())
 
 		// Start by systemd.
 		c := module.SystemdModuleConfig{
@@ -379,18 +382,27 @@ func StartComponent(getter ExecutorGetter, instances []meta.Instance) error {
 		}
 
 		if err != nil {
-			return errors.Annotatef(err, "failed to start: %s", ins.GetHost())
+			return errors.Annotatef(err, "failed to start: %s %s:%d",
+				ins.ComponentName(),
+				ins.GetHost(),
+				ins.GetPort())
 		}
 
 		// Check ready.
 		err = ins.Ready(e)
 		if err != nil {
-			str := fmt.Sprintf("\t%s failed to start: %s", ins.GetHost(), err)
+			str := fmt.Sprintf("\t%s %s:%d failed to start: %s",
+				ins.ComponentName(),
+				ins.GetHost(),
+				ins.GetPort(), err)
 			log.Errorf(str)
 			return errors.Annotatef(err, str)
 		}
 
-		log.Infof("\tStart %s success", ins.GetHost())
+		log.Infof("\tStart %s %s:%d success",
+			ins.ComponentName(),
+			ins.GetHost(),
+			ins.GetPort())
 	}
 
 	return nil
@@ -407,8 +419,9 @@ func StopMonitored(getter ExecutorGetter, instance meta.Instance, options meta.M
 		log.Infof("Stopping component %s", comp)
 
 		c := module.SystemdModuleConfig{
-			Unit:   fmt.Sprintf("%s-%d.service", comp, ports[comp]),
-			Action: "stop",
+			Unit:         fmt.Sprintf("%s-%d.service", comp, ports[comp]),
+			Action:       "stop",
+			ReloadDaemon: true,
 		}
 		systemd := module.NewSystemdModule(c)
 		stdout, stderr, err := systemd.Execute(e)
@@ -421,11 +434,17 @@ func StopMonitored(getter ExecutorGetter, instance meta.Instance, options meta.M
 		}
 
 		if err != nil {
-			return errors.Annotatef(err, "failed to stop: %s", instance.GetHost())
+			return errors.Annotatef(err, "failed to stop: %s %s:%d",
+				instance.ComponentName(),
+				instance.GetHost(),
+				instance.GetPort())
 		}
 
 		if err := meta.PortStopped(e, ports[comp]); err != nil {
-			str := fmt.Sprintf("\t%s failed to stop: %s", instance.GetHost(), err)
+			str := fmt.Sprintf("\t%s %s:%d failed to stop: %s",
+				instance.ComponentName(),
+				instance.GetHost(),
+				instance.GetPort(), err)
 			log.Errorf(str)
 			return errors.Annotatef(err, str)
 		}
@@ -474,17 +493,27 @@ func StopComponent(getter ExecutorGetter, instances []meta.Instance) error {
 		}
 
 		if err != nil {
-			return errors.Annotatef(err, "failed to stop: %s", ins.GetHost())
+			return errors.Annotatef(err, "failed to stop: %s %s:%d",
+				ins.ComponentName(),
+				ins.GetHost(),
+				ins.GetPort())
 		}
 
 		err = ins.WaitForDown(e)
 		if err != nil {
-			str := fmt.Sprintf("\t%s failed to stop: %s", ins.GetHost(), err)
+			str := fmt.Sprintf("\t%s failed to stop: %s %s:%d",
+				ins.ComponentName(),
+				ins.GetHost(),
+				ins.GetPort(),
+				err)
 			log.Errorf(str)
 			return errors.Annotatef(err, str)
 		}
 
-		log.Infof("\tStop %s success", ins.GetHost())
+		log.Infof("\tStop %s %s:%d success",
+			ins.ComponentName(),
+			ins.GetHost(),
+			ins.GetPort())
 	}
 
 	return nil
