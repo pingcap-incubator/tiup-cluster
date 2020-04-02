b61365b0d373a72ba610db4614f92eb36a8799cc
Yang Keao <keao.yang@yahoo.com>
2020-04-02 17:13:33 +0800

    add SQL check
    Signed-off-by: Yang Keao <keao.yang@yahoo.com>

--- tests/script/run.sh
+++ tests/script/run.sh
@@ -1,4 +1,4 @@
-#!/bin/sh 
+#!/bin/bash 
 
 eval $(ssh-agent) &> /dev/null
 ssh-add /root/.ssh/id_rsa &> /dev/null
@@ -6,6 +6,7 @@ ssh-add /root/.ssh/id_rsa &> /dev/null
 export PATH=$PATH:/usr/local/go/bin
 export PATH=$PATH:/tiops/bin
 
+if [[ "$1" == "prepare" ]]; then
 cat <<EOF >> /tmp/topology.yaml
 tidb_servers:
   - host: 172.19.0.101
@@ -33,80 +34,160 @@ cat <<EOF >> /tmp/scale-out-pd.yaml
 pd_servers:
   - host: 172.19.0.105
 EOF
+fi
 
-echo "Deploy and start"
-tiops deploy --key ~/.ssh/id_rsa test-cluster v3.0.12 /tmp/topology.yaml
-tiops start test-cluster
-sleep 5
+if [[ "$1" == "deploy" ]]; then
+echo "Deploy"
+yes|tiops deploy -i ~/.ssh/id_rsa test-cluster v3.0.12 /tmp/topology.yaml
+sleep 10
 tiops display test-cluster
+fi
 
+if [[ "$1" == "start" ]]; then
+tiops start test-cluster 
+sleep 10
+tiops display test-cluster
+fi
+
+if [[ "$1" == "scale-out-tidb" ]]; then
 echo "Scaling out TiDB"
-tiops scale-out --key ~/.ssh/id_rsa test-cluster /tmp/scale-out-tidb.yaml
-sleep 5
+yes|tiops scale-out -i ~/.ssh/id_rsa test-cluster /tmp/scale-out-tidb.yaml
+sleep 10
 tiops display test-cluster
-echo "Scaling out TiKV"
-tiops scale-out --key ~/.ssh/id_rsa test-cluster /tmp/scale-out-tikv.yaml
-sleep 5
-tiops display test-cluster
-echo "Scaling out PD"
-tiops scale-out --key ~/.ssh/id_rsa test-cluster /tmp/scale-out-pd.yaml
-sleep 5
-tiops display test-cluster
+fi
 
+if [[ "$1" == "scale-out-tikv" ]]; then
+echo "Scaling out TiKV"
+yes|tiops scale-out -i ~/.ssh/id_rsa test-cluster /tmp/scale-out-tikv.yaml
+sleep 10
+tiops display test-cluster
+fi
+
+if [[ "$1" == "scale-out-pd" ]]; then
+echo "Scaling out PD"
+yes|tiops scale-out -i ~/.ssh/id_rsa test-cluster /tmp/scale-out-pd.yaml
+sleep 10
+tiops display test-cluster
+fi
+
+if [[ "$1" == "restart-tidb" ]]; then
 echo "Restarting TiDB"
 tiops restart test-cluster --role tidb
-sleep 5
+sleep 10
 tiops display test-cluster
+fi
+
+if [[ "$1" == "restart-pd" ]]; then
 echo "Restarting PD"
 tiops restart test-cluster --role pd
-sleep 5
+sleep 10
 tiops display test-cluster
+fi
+
+if [[ "$1" == "restart-tikv" ]]; then
 echo "Restarting TiKV"
 tiops restart test-cluster --role tikv
-sleep 5
+sleep 10
 tiops display test-cluster
+fi
 
+if [[ "$1" == "stop-tidb" ]]; then
 echo "Stopping TiDB"
 tiops stop test-cluster --role tidb
-sleep 5
+sleep 10
 tiops display test-cluster
+fi
+
+if [[ "$1" == "stop-tikv" ]]; then
 echo "Stopping TiKV"
 tiops stop test-cluster --role tikv
-sleep 5
+sleep 10
 tiops display test-cluster
+fi
+
+if [[ "$1" == "stop-pd" ]]; then
 echo "Stopping PD"
 tiops stop test-cluster --role pd
-sleep 5
+sleep 10
 tiops display test-cluster
+fi
 
-echo "Starting"
-tiops start test-cluster
-sleep 5
-tiops display test-cluster 
+if [[ "$1" == "restart" ]]; then
 echo "Restarting"
 tiops restart test-cluster
-sleep 5
+sleep 10
 tiops display test-cluster
+fi
 
+if [[ "$1" == "upgrade" ]]; then
 echo "Upgrading"
 tiops upgrade test-cluster v4.0.0-beta.1
-sleep 5
+sleep 10
 tiops display test-cluster
+fi
 
+if [[ "$1" == "scale-in-pd" ]]; then
 echo "Scaling in PD"
-tiops scale-in test-cluster --node 172.19.0.102:2379
-sleep 5
+yes|tiops scale-in test-cluster --node 172.19.0.102:2379
+sleep 10
 tiops display test-cluster
-tiops restart test-cluster
-sleep 5
-tiops display test-cluster
-echo "Scaling in TiKV"
-tiops scale-in test-cluster --node 172.19.0.104:20160
-sleep 5
-tiops display test-cluster
-echo "Scaling in TiDB"
-tiops scale-in test-cluster --node 172.19.0.101:4000
-sleep 5
-tiops display test-cluster
+fi
 
-tiops destroy test-cluster
+if [[ "$1" == "scale-in-tikv" ]]; then
+echo "Scaling in TiKV"
+yes|tiops scale-in test-cluster --node 172.19.0.104:20160
+sleep 10
+tiops display test-cluster
+fi
+
+if [[ "$1" == "scale-in-tidb" ]]; then
+echo "Scaling in TiDB"
+yes|tiops scale-in test-cluster --node 172.19.0.104:4000
+sleep 10
+tiops display test-cluster
+fi
+
+if [[ "$1" == "destroy" ]]; then
+yes|tiops destroy test-cluster
+fi
+
+if [[ "$1" == "check-old-version" ]]; then
+  version=$(echo "SELECT @@version" | mysql -h 172.19.0.101 -u root -P 4000 | tail -n1)
+  if [[ ! "$version" == "5.7.25-TiDB-v3.0.12" ]]; then
+    echo "FAIL"
+    echo "version=$version"
+    exit 1
+  else
+    echo "SUCCESS"
+  fi
+fi
+if [[ "$1" == "check-new-version" ]]; then
+  version=$(echo "SELECT @@version" | mysql -h 172.19.0.101 -u root -P 4000 | tail -n1)
+  if [[ ! "$version" == "5.7.25-TiDB-v4.0.0-beta.1" ]]; then
+    echo "FAIL"
+    echo "version=$version"
+    exit 1
+  else
+    echo "SUCCESS"
+  fi
+fi
+if [[ "$1" == "check-prepare" ]]; then
+  echo "CREATE TABLE \`test_table\` (  \`col_double\` double DEFAULT NULL,  \`col_binary_8_key\` binary(8) DEFAULT NULL, KEY \`col_binary_8_key\` (\`col_binary_8_key\`) );"|mysql -h 172.19.0.101 -u root -P 4000 -D test
+  echo "INSERT INTO \`test_table\` VALUES(1000.0, \"lyashfuw\");" | mysql -h 172.19.0.101 -u root -P 4000 -D test
+  echo "SUCCESS"
+fi
+if [[ "$1" == "check-select" ]]; then
+  result=$(echo "SELECT \`col_double\` FROM \`test_table\` WHERE \`col_binary_8_key\`;show warnings;" | mysql -h 172.19.0.101 -u root -P 4000 -D test | tail -n1)
+  case $result in
+    Warning*1105*Eval*)
+      echo "SUCCESS"
+      ;;
+    Warning*1292*Truncated*)
+      echo "SUCCESS"
+      ;;
+    *)
+      echo "FAIL"
+      echo $result
+      ;;
+  esac
+fi
--- tests/script/test.sh
+++ tests/script/test.sh
@@ -1,4 +1,64 @@
 #!/bin/sh
 
+function run_test() {
+  docker exec -i tiops-control bash -c "/tmp/test.sh $1"
+}
+
+function check() {
+  local result=$(docker exec -i tiops-control bash -c "/tmp/test.sh check-$1")
+  if [ ! "$result" == "SUCCESS" ]; then
+    echo $result
+    exit 1
+  fi
+}
+
 cat run.sh | docker exec -i tiops-control bash -c "cat > /tmp/test.sh"
-docker exec -i tiops-control bash -c "chmod +x /tmp/test.sh && /tmp/test.sh"
+docker exec -i tiops-control bash -c "chmod +x /tmp/test.sh"
+
+run_test "prepare"
+run_test "deploy"
+run_test "start"
+check "prepare"
+check "old-version"
+check "select"
+
+run_test "scale-out-tidb"
+check "select"
+
+run_test "scale-out-tikv"
+check "select"
+
+run_test "scale-out-pd"
+check "select"
+
+run_test "restart-tidb"
+check "select"
+
+run_test "restart-pd"
+check "select"
+
+run_test "restart-tikv"
+check "select"
+
+run_test "stop-tidb"
+run_test "stop-tikv"
+run_test "stop-pd"
+
+run_test "restart"
+check "select"
+
+run_test "upgrade"
+check "select"
+
+run_test "scale-in-pd"
+run_test "restart"
+check "select"
+
+run_test "scale-in-tikv"
+run_test "restart"
+check "select"
+
+run_test "scale-in-tidb"
+check "select"
+
+run_test "destroy"
